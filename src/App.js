40ea5ee05715f258332764f2f03887d94a03edd3
alibahrani <alibahrani@mac.com>
2020-01-25 12:58:55 -0500

    Search field (#1)
    * Adding Search Field
    
    * Minor change onthe Search box component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {CardList} from './components/card-list/card-list.component'
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -8,23 +9,29 @@ class App extends Component {
     this.state = {
       products: [],
       searchField:''
-
     };
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+  
   componentDidMount() {
-    
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json()).then(somethings => this.setState({products:somethings}));
-    
   }
   
   render() {
+    const {products, searchField } = this.state
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       
       <div className="App">
-      <input type='search' onChange={e => this.setState({searchField: e.target.value} )} placeholder='Search products' />
-      <CardList products={this.state.products} />
+        <SearchBox 
+          placeholder="Search Product" 
+          handleChange={this.handleChange}/>
+        <CardList products={filteredProducts} />
         
       </div>
     );
